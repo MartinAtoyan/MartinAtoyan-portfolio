f45ddfdb60a1dc3cc1670b4810529f302c212df6
MartinAtoyan <martinwork31@gmail.com>
2024-06-02 12:31:44 +0400

    Add files via upload
    Lesson 3 homework

---
+++ 4.py
@@ -0,0 +1,15 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = "Python"
+>>> count = 0
+>>> for i in x:
+...     print(i, " : ", count)
+...     count += 1
+... 
+...     
+P  :  0
+y  :  1
+t  :  2
+h  :  3
+o  :  4
+n  :  5
---
+++ 5.py
@@ -0,0 +1,21 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n1 = int(input())
+6
+>>> n2 = int(input())
+3
+>>> sym = input()
+/
+>>> if sym == "+":
+...     print(n1+n2)
+... elif sym == "-":
+...     print(n1-n2)
+... elif sym == "*":
+...     print(n1*n2)
+... elif (sym == "/") and (n2 != 0):
+...     print(n1 / n2)
+... else:
+...     print("An incorrect action was selected")
+... 
+...     
+2.0
