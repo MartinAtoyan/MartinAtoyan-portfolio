6f3d266b073400d861c3a083b609fcde162c4892
MartinAtoyan <martinwork31@gmail.com>
2024-06-12 17:56:54 +0400

    Add files via upload

---
+++ ls7_1.py
@@ -0,0 +1,4 @@
+password = "abcdefgh"
+input_password = input()
+res = "valid password" if input_password == password else "invalid password"
+print(res)
---
+++ ls7_2.py
@@ -0,0 +1,13 @@
+list_prime = []
+
+for num in range(2, 101):
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        list_prime.append(num)
+
+print(list_prime)
+
