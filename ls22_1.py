f2f3ae4b4381888d0933ec595fd83f9836ecd686
MartinAtoyan <martinwork31@gmail.com>
2024-07-12 12:51:26 +0400

    Add files via upload

---
+++ ls22_1.py
@@ -0,0 +1,8 @@
+def foo(n):
+    while(n >= 0):
+        print(n)
+        n -= 1
+
+n = int(input())
+print()
+foo(n)
---
+++ ls22_3.py
@@ -0,0 +1,6 @@
+def print_list(ls):
+    for i in range(len(ls)):
+        print(ls[i])
+
+list = [1, 2, 3, 4, 5]
+print_list(list)
