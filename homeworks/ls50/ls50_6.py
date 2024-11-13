97af26f97c9a7581f9543e4d6074277d7333fca9
Martin <martinwork31@gmail.com>
2024-11-13 17:34:59 +0400

    yaml, post, request (update)

--- homeworks/ls50/ls50_6.py
+++ homeworks/ls50/ls50_6.py
@@ -5,7 +5,7 @@ data = {"tile": "test", "body":"Test body", "userID":1}
 
 response = requests.post(url, data)
 
-if response.status_code == 200:
+if response.status_code == 201:
     print("test pass", response.json())
 else:
     print("fail", response.status_code)
